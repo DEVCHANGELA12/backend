d02e4189495939ed3f790fced6070503d1c7898c
Administrator <learnwithparth.in@gmail.com>
2022-07-26 10:32:01 +0530

    Added Student Entity, Student Repository.

--- src/main/java/in/ac/charusat/studentmgmtsystem/model/Student.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/model/Student.java
@@ -1,6 +1,12 @@
-package in.ac.charusat.studentmgmtsystem.controller;
+package in.ac.charusat.studentmgmtsystem.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
     private Integer id;
     private String name;
     private String address;
@@ -9,6 +15,10 @@ public class Student {
         this.id = id;
         this.name = name;
         this.address = address;
+    }
+
+    public Student() {
+
     }
 
     public Integer getId() {
