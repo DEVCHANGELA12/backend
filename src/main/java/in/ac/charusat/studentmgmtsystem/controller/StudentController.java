a0c7dc3b338ceddfb6340b9a7d02a583ce064ba9
Administrator <learnwithparth.in@gmail.com>
2022-08-01 14:05:59 +0530

    Added cross origin

--- src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins="https://app-dummy123.herokuapp.com")
 public class StudentController {
 
     @Autowired
