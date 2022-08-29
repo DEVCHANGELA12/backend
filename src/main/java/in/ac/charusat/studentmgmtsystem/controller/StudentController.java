671806bdd22b4291dacdf5ab90ffc2cf7892193b
learnwithparth <68218484+learnwithparth@users.noreply.github.com>
2022-08-29 12:25:55 +0530

    Added security with inbuilt user.

--- src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
+++ src/main/java/in/ac/charusat/studentmgmtsystem/controller/StudentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins="https://app-dummy123.herokuapp.com")
+@CrossOrigin(origins="http://localhost:3000")
 public class StudentController {
 
     @Autowired
@@ -29,30 +29,30 @@ public class StudentController {
     @GetMapping("/listStudents")
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
-    }
+    } // Select * from student;
 
     // Get the student information
     @GetMapping("/student/{id}")
     public Student getStudent(@PathVariable Integer id) {
         return studentRepository.findById(id).get();
-    }
+    } // Select * from student where id=?
 
     // Delete the student
-    @DeleteMapping("/student/{id}")
+    @DeleteMapping("/student/{id}") // delete from student where id=?
     public List<Student> deleteStudent(@PathVariable Integer id) {
         studentRepository.delete(studentRepository.findById(id).get());
         return studentRepository.findAll();
     }
 
     // Add new student
-    @PostMapping("/student")
+    @PostMapping("/student") // insert into student values(?, ?, ?)
     public List<Student> addStudent(@RequestBody Student student) {
         studentRepository.save(student);
         return studentRepository.findAll();
     }
 
     // Update the student information
-    @PutMapping("/student/{id}")
+    @PutMapping("/student/{id}") // update table student set name=? where id=?
     public List<Student> updateStudent(@RequestBody Student student, @PathVariable Integer id) {
         Student studentObj = studentRepository.findById(id).get();
         studentObj.setName(student.getName());
